05a5bd29a09d010e1a39e2c62fc03b7154e104c5
hr-repo <hardikcollage2025@gmail.com>
2026-04-24 23:56:10 +0530

    Add files via upload

---
+++ ex3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Character :- ");
+        char ch = sc.next().charAt(0);
+
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+            System.out.println("It is a Vowel");
+        } else if ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')) {
+            System.out.println("It is a Consonant");
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
+}
