f11064cd52875628d3d56ee48792e3530f5518a7
hr-repo <hardikcollage2025@gmail.com>
2026-04-25 10:45:48 +0530

    Add files via upload

---
+++ ex7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ex7 {
+    private String employeeName;
+    private double employeeSalary;
+
+    public void readEmployeeData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee Name :- ");
+        employeeName = sc.nextLine();
+        System.out.print("Enter Employee Salary :- ");
+        employeeSalary = sc.nextDouble();
+    }
+
+    public void displayEmployeeData() {
+        System.out.println("Employee Name :- " + employeeName);
+        System.out.println("Employee Salary :- " + employeeSalary);
+    }
+
+    public static void main(String[] args) {
+        ex7 e = new ex7();
+        e.readEmployeeData();
+        e.displayEmployeeData();
+    }
+}
